25a647eaca1d98e7ffeb8a94f5b836f5d49a342c
imrebartis <imbartis@gmail.com>
2017-08-26 12:30:30 +0300

    new GoogleStrategy options set up

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const keys = require('./config/keys');
+
 const app = express();
 
 // console.developers.google.com
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      // the route users will be sent to after they grant permission to the authentication
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
