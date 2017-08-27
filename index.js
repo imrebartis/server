5f67782d48e9109b05fe5dc204625841691f0bbe
imrebartis <imbartis@gmail.com>
2017-08-27 06:24:34 +0300

    stage one of authentication proces (the one with passport) done

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ passport.use(
       // the route users will be sent to after they grant permission to the authentication
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile:', profile);
     }
   )
 );
