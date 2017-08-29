56e99105486514636ec19cc68c73a034f107828b
imrebartis <imbartis@gmail.com>
2017-08-29 07:23:02 +0300

    user model created

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+// this is the same as saying const Schema = mongoose.Schema
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
