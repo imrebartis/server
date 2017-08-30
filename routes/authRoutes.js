64b8e44d5cea2f28a643e5326c2f0e14556a50e6
imrebartis <imbartis@gmail.com>
2017-08-30 18:31:06 +0300

    logout works

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,13 @@ module.exports = app => {
   // Google sees 'code' in URL, replies with details about user
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
   // auth handler
+  // http://localhost:5000/api/current_user will show id & googleId of user
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
