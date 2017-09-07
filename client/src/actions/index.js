9d34a672da515b68d994d90244e1abf3688b58fb
imrebartis <imbartis@gmail.com>
2017-09-07 07:24:59 +0300

    redux thunk set up started

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 const fetchUser = () => {
-  // the route is same as in the auth handler in authRoutes.js
-  axios.get('/api/current_user');
+  // the dispatch function will automatically forward all the actions passed in to the different reducers
+  // AFTER the get current user api request has been completed
+  return function(dispatch) {
+    axios
+      // the route is same as in the auth handler in authRoutes.js
+      .get('/api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
