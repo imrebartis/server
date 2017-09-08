921130052f341f185e3de4a025b0a44e42f4db03
imrebartis <imbartis@gmail.com>
2017-09-08 07:34:45 +0300

    auth reducer returns null (don't know if logged in or not), user model or false (i.e. logged out)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,5 +5,5 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   // the dispatch function will automatically forward all the actions passed in to the different reducers
   // AFTER the get current user api request has been completed
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,11 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_USER } from '../actions/types';
+
+// state = null means by default we don't know if user is logged in or not
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      // return the user model or false (i.e. not logged in)
+      return action.payload || false;
     default:
       return state;
   }
