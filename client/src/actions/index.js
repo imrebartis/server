e6d7aafee5b0bafb50b159a54973f96878a38971
imrebartis <imbartis@gmail.com>
2017-09-08 07:10:30 +0300

    updated const store in client/src/index.js (should've done this in the previous commit); refactored client/src/actions/index.js to async/await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
   // the dispatch function will automatically forward all the actions passed in to the different reducers
   // AFTER the get current user api request has been completed
-  return function(dispatch) {
-    axios
-      // the route is same as in the auth handler in authRoutes.js
-      .get('/api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+  dispatch({ type: FETCH_USER, payload: res });
 };
