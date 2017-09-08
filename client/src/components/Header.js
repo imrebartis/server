bf5da9016b58bf0ab58c3519df566a16bdab363d
imrebartis <imbartis@gmail.com>
2017-09-08 07:57:59 +0300

    header content updated

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+// hook up component to the redux store step 1
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      // still deciding if logged in or not
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    // console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>
-              <a>Login With Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+// hook up component to the redux store step 2
+export default connect(mapStateToProps)(Header);
