911d34ddf66976fd6fdbcc2f4ccd12c30473491d
imrebartis <imbartis@gmail.com>
2017-09-12 06:35:03 +0300

    minor fixes to payments

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,12 +5,16 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         // token here is actually an object that contains i.a. the id of the pending charge
         // plus the data given by client in the stripe "pay with card" form
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
