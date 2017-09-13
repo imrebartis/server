f7aea13f5606d295189f70dab1b5bea523b18b15
imrebartis <imbartis@gmail.com>
2017-09-13 07:34:38 +0300

    post request handlers set up; npm install --save stripe (in the server folder); npm install --save body-parser (server folder); bodyParser middleware added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-// User model has to be required before passport
+// User model has to be required before passport.js
 require('./models/User');
 require('./services/passport');
 
@@ -12,6 +13,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 // middlewares
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     // cookie will last 30 days before it expires
@@ -23,7 +25,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// the required files return a function that we immediately call with the express app object
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
