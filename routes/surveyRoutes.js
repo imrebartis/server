f989898df4adbb1a71f6e1ab9210835085672ea8
imrebartis <imbartis@gmail.com>
2017-09-20 06:25:36 +0300

    survey created

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      // transforming the list of strings ( = email addresses) into an array of strings with split
+      // then creating a new array of objects out of the array of strings with map
+      // BTW (email => ({email:email.trim() })) is the same as (email => {return {email:email.trim()}})
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
+  });
 };
