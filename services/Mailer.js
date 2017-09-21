3a213cae92399b54688510a4afff0dc1138a0995
imrebartis <imbartis@gmail.com>
2017-09-21 07:17:49 +0300

    mailer constructor added

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,6 +4,18 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+    // expecting to have as first argument any object containing a subject & recipients
+    // meaning it doesn't necessarily have to be the survey
+    // so we can re-use this in the future with something else then survey
+    constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('no-reply@emaily.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
