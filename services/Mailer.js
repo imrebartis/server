45aac0029aaa1cbd3224d62edd2c74c84474b90d
imrebartis <imbartis@gmail.com>
2017-09-21 06:53:28 +0300

    mailer class added

---
+++ services/Mailer.js
@@ -0,0 +1,7 @@
+// the file name's with capital M because it exports a class
+
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {}
