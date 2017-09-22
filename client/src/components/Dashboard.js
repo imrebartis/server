f531254e017f2c37de7066ae812039761f07f204
imrebartis <imbartis@gmail.com>
2017-09-22 07:49:59 +0300

    material icons added

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <div>
-      Dashboard
+       Dashboard
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </a>
+      </div>
     </div>
   );
 };
