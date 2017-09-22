6f275ebb93aeebc74e4c0a3b4624a97e7a1dc674
imrebartis <imbartis@gmail.com>
2017-09-22 07:06:12 +0300

    added error checking & async/await to survey route handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -24,6 +24,18 @@ module.exports = app => {
 
     // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    await mailer.send();
+    
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      // 422 = unprocessable entity, i.e. sth's wrong with the data u sent us
+      res.status(422).send(err);
+    }
   });
 };
