382ee9380bea5830d85bec4cea284ed13e5c45f7
imrebartis <imbartis@gmail.com>
2017-09-24 09:29:20 +0300

    redux form set up using reduxForm's Field  component

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,10 +1,18 @@
 // SurveyForm shows a form for a user to add input
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+// Field will help us render any type of html element that collects input from users
+import { reduxForm, Field } from 'redux-form';
 
 class SurveyForm extends Component {
   render() {
-    return <div>SurveyForm!</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field component="input" type="text" name="SurveyTitle" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
