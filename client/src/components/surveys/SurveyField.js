69dff246649f9f29e7823f03be369da9b40ff7af
Imre Bartis <imre.bartis@siili.com>
2017-10-20 11:23:59 +0300

    all fields added

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,12 @@
 // label and text input
 import React from 'react';
 
-export default ({input}) => {
+export default ({input, label}) => {
 // return all the values and keys (i.e. event handlers like onChange, onBlur, etc.) that are in input
 // {... input} is a shortcut for writing onBlur={input.OnBlur}, onChange={input.onChange}, etc.
   return (
     <div>
+      <label>{label}</label>
       <input {... input}/>
       </div>
   );
