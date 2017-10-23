4d64e6b64fa2f84290d60e9f773315f950696874
Imre Bartis <imre.bartis@siili.com>
2017-10-23 12:30:25 +0300

    validation error for title field ok

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,13 +2,15 @@
 // label and text input
 import React from 'react';
 
-export default ({input, label}) => {
+export default ({input, label, meta: {error, touched}}) => {
+ // console.log(meta);
 // return all the values and keys (i.e. event handlers like onChange, onBlur, etc.) that are in input
 // {... input} is a shortcut for writing onBlur={input.OnBlur}, onChange={input.onChange}, etc.
   return (
     <div>
       <label>{label}</label>
       <input {... input}/>
+      {touched && error}
       </div>
   );
 };
