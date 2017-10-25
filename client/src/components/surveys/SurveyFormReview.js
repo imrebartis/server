f9be955a96f8114b80b6e9a20bcbd7bc2fabfa68
Imre Bartis <imre.bartis@siili.com>
2017-10-25 11:44:55 +0300

    component state added to handle visibility of SurveyFormReview

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,12 @@
+// SurveyFormReview shows users their form inputs for review
+import React from 'react';
+
+const SurveyFormReview = () => {
+    return (
+      <div>
+        <h5>Please confirm your entries</h5>
+      </div>
+    );
+  };
+
+export default SurveyFormReview;
