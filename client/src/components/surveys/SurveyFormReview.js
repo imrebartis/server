52d1ec825a18bb910dde9e365c6002fb8dc27733
Imre Bartis <imre.bartis@siili.com>
2017-10-30 11:42:06 +0200

    formFields refactoring part 2

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,5 +1,5 @@
 // SurveyFormReview shows users their form inputs for review
-// import _ from 'lodash';
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
@@ -7,9 +7,21 @@ import formFields from './formFields';
 // SurveyFormReview shows users their form inputs for review
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    );
+  });
+  
+  return (
       <div>
         <h5>Please confirm your entries</h5>
+        {reviewFields}
         <button
           className="yellow darken-3 white-text btn-flat"
           onClick={onCancel}
