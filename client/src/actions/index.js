c43e52d45d990fac8cce2b083b3e9a095dccb67e
Imre Bartis <imre.bartis@siili.com>
2017-11-02 11:58:52 +0200

    posting to surveys ok

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,12 +9,13 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitSurvey = values => {
-  return { type: ' submit_survey '};
-};
-
 export const handleToken = token => async dispatch => {
   const res = await axios.post('/api/stripe', token);
 
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = values => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
   dispatch({ type: FETCH_USER, payload: res.data });
 };
