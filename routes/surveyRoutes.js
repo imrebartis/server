8ae0fc44f0b9d6856259b3227bf51ea86229441c
Imre Bartis <imre.bartis@siili.com>
2017-11-06 14:32:41 +0200

    list of recipients won't be included in list of surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,7 +11,10 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.get('/api/surveys', requireLogin, async (req, res) => {
-    const surveys = await Survey.find({ _user: req.user.id });
+    // mongoose code (see http://mongoosejs.com/docs/api.html):
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
 
     res.send(surveys);
   });
